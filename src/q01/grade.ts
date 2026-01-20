ddf0b824f558f95a0d98253041f98627dc0a77ad
Kankawee Seetalarom <132635060+Meepython@users.noreply.github.com>
2026-01-20 18:09:13 +0700

    q1

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -1,30 +1,19 @@
-import { isStringObject } from "node:util/types";
+const score = parseFloat(process.argv[2]);
 
-export{}
-
-let input:number = Number(process.argv[2]);
-
-if (isNaN(input) || input < 0 || input > 100) { 
-    console.log('Invalid Input');
-    process.exit(1);
-} else if (typeof input === "string") {
-    console.log('Invalid Input');
-    process.exit(1);
+if(isNaN(score) || score < 0||score>100){
+  console.log("Invalid Input");
+  process.exit(0);
+}else{
+  if(score>=80){
+    console.log("Grade is A");
+  }else if (score>=70){
+    console.log("Grade is B");
+  }else if (score>=60){
+    console.log("Grade is C")
+  }else if (score>=50){
+    console.log("Grade is D")
+  }
+  else{
+    console.log("Grade is F")
+  }
 }
-
-let grade:number = input;
-
-if (grade >= 80) {
-    console.log('Grade is A');
-} else if (grade >= 70) {
-    console.log('Grade is B');
-} else if (grade >= 60) {
-    console.log('Grade is C');
-} else if (grade >= 50) {
-    console.log('Grade is D');
-} else if (grade < 50) {
-    console.log('Grade is F')
-} else {
-    console.log('Invalid Input');
-}
-    
