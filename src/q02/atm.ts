703beccc65120cfc5bb7e3b0b8aeab6f7e40506d
Kankawee Seetalarom <132635060+Meepython@users.noreply.github.com>
2026-01-20 20:10:49 +0700

    ww

--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -6,6 +6,11 @@ const WithdrawalAmountInput:string = process.argv[3]
 const AccountBalance:number = Number(AccountBalanceInput)
 const WithdrawalAmount:number = Number(WithdrawalAmountInput)
 
+if (AccountBalanceInput.trim() === "" || WithdrawalAmountInput.trim() === "" || AccountBalanceInput === undefined || WithdrawalAmountInput === undefined) {
+    console.log("Invalid Input")
+    process.exit(1)
+}
+
 if (Number.isNaN(AccountBalance) || Number.isNaN(WithdrawalAmount)) {
     console.log("Invalid Input")
     process.exit(1)
