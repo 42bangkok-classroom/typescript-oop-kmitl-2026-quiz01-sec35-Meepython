33a4fa1667df70ca9c27029a2b1d43045167a058
Kankawee Seetalarom <132635060+Meepython@users.noreply.github.com>
2026-01-20 18:31:26 +0700

    q1-2

--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -1,17 +1,20 @@
-export{}
+export {}
 
-let ab = Number(process.argv[2]); // Account Balance
-let wa = Number(process.argv[3]); // Withdrawal Amount
+const AccountBalanceInput = process.argv[2]
+const WithdrawalAmountInput = process.argv[3]
 
-if (isNaN(ab) || isNaN(wa) || ab === undefined || wa === undefined) {
-    console.log('Invalid Input');
-    process.exit(0);
+if (!AccountBalanceInput || !WithdrawalAmountInput) {
+    console.log("Invalid Input")
+    process.exit(1)
 }
 
-if (wa > ab) {
-    console.log('Insufficient balance');
-} else if (wa > 5000) {
-    console.log('Exceeds per-withdrawal limit');
+const AccountBalance = Number(AccountBalanceInput)
+const WithdrawalAmount = Number(WithdrawalAmountInput)
+
+if (WithdrawalAmount > AccountBalance) {
+    console.log("Insufficient balance")
+} else if (WithdrawalAmount > 5000) {
+    console.log("Exceeds per-withdrawal limit")
 } else {
-    console.log('Withdrawal approved')
+    console.log("Withdrawal approved")
 }
